40880088e846139d0394366f9e520e15f8c28fe0
AshikMuhammed123 <ashiksaads2018@gmail.com>
2025-10-25 08:36:00 +0530

    day 8 work

--- mywebsite/main/views.py
+++ mywebsite/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here
+# Create your views here.
 from django.shortcuts import render
 
 def home(request):
@@ -8,3 +8,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
